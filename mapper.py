eddcf95c23a5a5802df60c2b6d04af1e332769a7
Jose Luis <joche200103@gmail.com>
2023-08-31 08:44:33 -0500

    Mapper structure added

--- mapper.py
+++ mapper.py
@@ -1,2 +1,35 @@
 # leer mínimo un chunk
 # flags
+
+class Map:
+    has_ended: bool
+    failure: bool
+    chuck_identifier: str
+    max_retries: int
+
+    def __init__(self):
+        self.chuck_identifier = None
+        self.failure = False
+        self.has_ended = False
+
+    def run(self):
+        while True:
+            if self.has_ended:
+                break
+            if self.failure:
+                self.failure = False
+            mapped_data = self.process_chunk()
+            print(mapped_data)   #Debug
+
+    def process_chunk(self):
+        path = self.get_chunk_path()
+        File = open(path)
+        mapped_data = []
+        for line in File:
+            words = line.split()
+            for word in words:
+                mapped_data.append((word, 1))
+        return mapped_data
+
+    #def get_chunk_path(self):
+        #Logica para obtener el path del chunk
